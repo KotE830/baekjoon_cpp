9fe09c9b614618ed1dc355192cca386090c7dbf2
KotE <ham10233@naver.com>
2024-01-19 21:39:33 +0900

    문자와 문자열

--- codes/27866.cpp
+++ codes/27866.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int main() {
-  string s;
-  int i;
+    string s;
+    int i;
 
-  cin >> s >> i;
-  cout << s[i-1];
+    cin >> s >> i;
+    cout << s[i-1];
 
-  return 0;
+    return 0;
 }
