39a27007d681fd049358c0a9da8ca2e59bc8ddfc
KotE <ham10233@naver.com>
2024-01-29 21:58:00 +0900

    <cmath>

--- tech/include/cmath.cpp
+++ tech/include/cmath.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-  double d = 4.5;
-  
-  ceil(d);  // 4
-  floor(d); // 5
-  round(d); // 5
+    double d = 4.5;
 
-  return 0;
+    ceil(d);  // 4
+    floor(d); // 5
+    round(d); // 5
+
+    return 0;
 }
