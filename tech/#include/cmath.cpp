7179ec201d96194906bece7f78f0e2f8100af65f
KotE <ham10233@naver.com>
2024-02-07 23:27:25 +0900

    <cmath>

--- tech/#include/cmath.cpp
+++ tech/#include/cmath.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 int main() {
     double d = 4.5;
+    int i = 9;
 
     ceil(d);  // 4
     floor(d); // 5
     round(d); // 5
+
+    pow(i, 2); // 81
+    sqrt(i);   // 3
 
     return 0;
 }
