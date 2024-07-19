731e7b16b5dd602690b4f72e3924004509e36adc
KotE <ham10233@naver.com>
2024-07-20 00:49:39 +0900

    붙임성 좋은 총총이

---
+++ codes/Silver/26069.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, i, result = 0;
+    cin >> n;
+
+    set<string> dance{"ChongChong"};
+    for (i = 0; i < n; i++) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        if (dance.find(s1) != dance.end()) {
+            dance.insert(s2);
+        } else if (dance.find(s2) != dance.end()) {
+            dance.insert(s1);
+        }
+    }
+
+    cout << dance.size();
+
+    return 0;
+}
